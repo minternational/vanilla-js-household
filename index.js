0b929d37025b59777f458856682c46774704e16c
minternational <mintkuehn@gmail.com>
2020-02-15 15:20:54 +0100

    added post method

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require("express")
 const bodyparser = require("body-parser")
 const app = express()
 const path = require("path")
+const hausHaltsBuch = {
+  artikel: [],
+  preis: []
+}
+
+app.use(bodyparser.urlencoded({
+  extended: false
+}))
 
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.send('index.html')
+})
+
+app.post("/", (req, res, next) => {
+  hausHaltsBuch.artikel.push(req.body.artikel)
+  hausHaltsBuch.preis.push(req.body.preis)
+    res.redirect("/")
 })
 
 app.listen(3001)
